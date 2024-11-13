32fc2712ac68dcaf0d18f279415c5939e6589b62
makemile <mkengya@gmail.com>
2024-11-13 00:54:57 -0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import imageBgBody from "./assets/image.jpg";
+function App() {
+  return (
+    <div>
+        <img src={imageBgBody} className="w-full max-h-min" />
+    </div>
+  );
+}
+
+export default App;
